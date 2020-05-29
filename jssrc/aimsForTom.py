5e0d63b69aaad6b92783bc24ffb69554273ddf20
arg0n_50d1um-V4naD1um <51513216+argonsodiumvanadium@users.noreply.github.com>
2020-05-29 14:48:40 +0530

    Delete aimsForTom.py

--- jssrc/aimsForTom.py
+++
@@ -1,2 +0,0 @@
-print("AIM : Complete the me and user render pane by finishing all powers (except root and settings)\nSTATUS : \u001B[91mNot Done\u001B[0m")
-print("AIM : Complete the events and Activity pane (can be done in a day)\nStatus : \u001B[91mNot Done\u001B[0m")
